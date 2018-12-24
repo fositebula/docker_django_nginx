cb533c0d9d1203aea293103648138631640ab38d
dongpl <dongpl@spreadst.com>
2018-12-24 14:24:39 +0800

    用supervisor启动celery worker

--- vts_test/start.sh
+++ vts_test/start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #命令只执行最后一个,所以用 &&
+#python manage.py collectstatic --noinput
+#python manage.py makemigrations
+#python manage.py migrate --fake pay zero
+#python manage.py showmigrations
+#python manage.py migrate --fake pay zero
+#python manage.py makemigrations
+#python manage.py migrate --fake-initial
 python manage.py collectstatic --noinput
 python manage.py makemigrations
-python manage.py migrate --fake pay zero
-python manage.py showmigrations
-python manage.py migrate --fake pay zero
-python manage.py makemigrations
-python manage.py migrate --fake-initial
-#celery -A vts_test worker --loglevel=info
+python manage.py migrate
 gunicorn vts_test.wsgi:application -c gunicorn.conf
